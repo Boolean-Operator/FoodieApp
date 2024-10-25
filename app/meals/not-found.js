800a3eebfc33b9957afa00fead0b083af82f3ab2
Mark Graybill <marktgraybill@gmail.com>
2024-10-25 15:20:18 -0400

    add meal detail page, data request logic and not found component

---
+++ app/meals/not-found.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Meal Not Found</h1>
+      <p>
+        I'm sorry, Michael, but I was unable to find the individual page or meal
+        information you requested.
+      </p>
+    </main>
+  );
+}
