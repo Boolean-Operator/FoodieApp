c12efc90676edc71fe4a878de69d649bc414153e
Mark Graybill <marktgraybill@gmail.com>
2024-10-25 15:17:49 -0400

    update nav link hover and active css, add not found page to main app

---
+++ app/not-found.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>NotFound</h1>
+      <p>
+        I'm sorry, Michael, but I was unable to find the page or resource you
+        requested.
+      </p>
+    </main>
+  );
+}
